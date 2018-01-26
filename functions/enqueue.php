59991cc9a46449bfdccb75393d50494f3afd617b
Daxc <daxc@rain.agency>
2018-01-26 17:23:46 -0600

    Removed custom js and css since it will be loaded in the child theme

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -12,7 +12,7 @@ add_action( 'wp_enqueue_scripts', 'dax_blank_register_scripts', 999 );
 if ( ! function_exists( 'dax_blank_register_styles' ) ) :
 	function dax_blank_register_styles()
 	{
-		wp_register_style('styles', get_template_directory_uri() . '/assets/css/styles.css', array(), '1.0.0', 'all');
+		wp_register_style('styles', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
 		wp_enqueue_style('styles');
 		wp_register_style('fontawesome', '//use.fontawesome.com/releases/v5.0.4/js/all.js', array(), '5.0.4', 'all');
 		wp_enqueue_style('fontawesome');
@@ -28,13 +28,7 @@ endif;
 if ( ! function_exists( 'dax_blank_register_scripts' ) ) :
 
 	function dax_blank_register_scripts() {
-
 		wp_deregister_script( 'jquery' );
-
 		wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), '3.3.1', false );
-
-		// Custom scripts loaded at the footer. jQuery is required in the array.
-		wp_enqueue_script( 'dax_blank_custom_js', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.0.0', true );
-
 	}
 endif;

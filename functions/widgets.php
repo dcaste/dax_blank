05c380c92abc8511377d5e513a9caefe60643166
Daxc <daxc@rain.agency>
2018-01-26 17:20:15 -0600

    Cleaned up the widgets area

--- functions/widgets.php
+++ functions/widgets.php
@@ -5,36 +5,23 @@
  * @package dax_blank
  */
 
-if ( ! function_exists( 'dax_blank_sidebars' ) ) {
+if ( ! function_exists( 'dax_blank_sidebars' ) ) :
 
 	function dax_blank_sidebars() {
 		$args = array(
-			'id'            => 'sidebar_1',
-			'class'         => 'sidebar_1',
-			'name'          => __( 'Sidebar 1', 'dax_blank' ),
+			'class'         => 'widget_area_1',
+			'name'          => __( 'Widget Area 1', 'dax_blank' ),
 			'before_title'  => '<h2 class="widget-title">',
 			'after_title'   => '</h2>',
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
 		);
 		register_sidebar( $args );
 
-		$args = array(
-			'id'            => 'sidebar_2',
-			'class'         => 'sidebar_2',
-			'name'          => __( 'Sidebar 2', 'dax_blank' ),
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-		);
-		register_sidebar( $args );
-
-	} // Ends Sidebars function.
-
+	}
 	add_action( 'widgets_init', 'dax_blank_sidebars' );
 
-} // End if Sidebars function exists.
+endif;
 
 // Allow shortcodes in widgets.
 add_filter('widget_text', 'do_shortcode');
